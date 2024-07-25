2b4e2e2b5204ac2f8f3b94cfd6ca4556ab4ee7ef
Matteo Arcari <matteoarcari06@gmail.com>
2024-07-25 14:23:43 +0200

    james bound

--- schedulers/claim/scheduler.cpp
+++ schedulers/claim/scheduler.cpp
@@ -8,12 +8,13 @@
 #include <limits.h>
 #include <map>
 #include <iostream>
+#include <functional>
 
 using namespace std;
 using vi = vector<int>;
 
 int cores, p, n;
-vi indeg, w, core_time, free_time, free_nodes, max_depth;
+vi indeg, w, core_time, free_time, free_nodes, max_depth, bound;
 
 vector<array<int, 3>> res;
 vector<array<int, 3>> act;
@@ -29,6 +30,7 @@ void solve(int pos = 0) {
             end = max(end, e);
         }
         if (end < best_end) {
+            cerr << end << endl;
             best_end = end;
             res = act;
         }
@@ -52,6 +54,7 @@ void solve(int pos = 0) {
         act[i] = {start, end, c};
         
         if (start + max_depth[i] >= best_end) continue; // do not recurr if actual max time is > best_end
+        if (start + bound[i] >= best_end) continue; // do not recurr if actual max time is > best_end
 
         int prev_time = core_time[c];
         core_time[c] = end;
@@ -84,6 +87,41 @@ void solve(int pos = 0) {
     }
 }
 
+void james_bound(int n) {
+    vi chain(n), vis(n), sum(n);
+    std::function<void(int)> calc_chain = [&](int v){
+        chain[v] = w[v];
+        vis[v] = 1;
+        for(int u : adj[v]){
+            if(!vis[u]) calc_chain(u);
+            chain[v] = max(chain[v], chain[u] + w[v]);
+        }
+    };
+    for(int i = 0; i < n; i++) {
+        if (!vis[i]) {
+            calc_chain(i);
+        }
+    }
+    
+    function<void(int, int)> calc_sum = [&](int v, int p){
+        sum[p] += w[v];
+        vis[v] = 1;
+        for (int u : adj[v]) {
+            if (!vis[u]) calc_sum(u, p);
+        }
+    };
+    for (int i = 0; i < n; i++) {
+        sum[i] = 0;
+        vis.assign(n, 0);
+        calc_sum(i, i);
+    }
+
+    bound.resize(n);
+    for(int i = 0; i < n; i++){
+        bound[i] = max(chain[i], (sum[i] - 1) / cores + 1);
+    }
+}
+
 vector<array<int, 3>> schedule(int n, int m, int p, int y, vi w, vi a, vi b) {
     ::w = w;
     ::p = 0;
@@ -103,6 +141,7 @@ vector<array<int, 3>> schedule(int n, int m, int p, int y, vi w, vi a, vi b) {
             free_nodes.push_back(i);
         }
     }
+    james_bound(n);
     {
         max_depth.resize(n);
         vi toposort, ind = indeg;
